9a0aa667e3539fabfeaa59c6168703ca4dc35b23
PansonPanson <PansonPanson@163.com>
2018-10-18 18:52:20 +0800

    CS:APP chapter02 note

---
+++ jvm/src/main/java/com/panson/jvm/JvmApplication.java
@@ -0,0 +1,12 @@
+package com.panson.jvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvmApplication.class, args);
+	}
+}
