e5190871251c9206134e1fb4f26fd55631fbc471
PansonPanson <PansonPanson@163.com>
2018-11-15 16:45:17 +0800

    array

---
+++ algorithm/array/构建乘积数组.java
@@ -0,0 +1,26 @@
+/*
+给定一个数组A[0,1,...,n-1],请构建一个数组B[0,1,...,n-1],
+其中B中的元素B[i]=A[0]*A[1]*...*A[i-1]*A[i+1]*...*A[n-1]。
+不能使用除法。
+*/
+import java.util.ArrayList;
+public class Solution {
+    public int[] multiply(int[] A) {
+        int length = A.length;
+        int[] B = new int[length];
+        if (length != 0) {
+            B[0] = 1;
+        }
+        // 下三角连乘
+        for (int i = 1; i < length; i++) {
+            B[i] = B[i -1] * A[i - 1];
+        }
+        // 上三角连乘
+        int temp = 1;
+        for (int j = length - 2; j >= 0; j--) {
+            temp = temp * A[j + 1];
+            B[j] = B[j] * temp;
+        }
+        return B;
+    }
+}
