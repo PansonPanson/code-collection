daa3a913804af2317fb768b0f9fec22d9400ab1d
panxing <panxing@deepwise.com>
2020-10-26 22:53:46 +0800

    auto commit

---
+++ jvm/src/main/java/top/panson/jvm/youngc/Youggc.java
@@ -0,0 +1,19 @@
+package top.panson.jvm.youngc;
+
+import java.util.ArrayList;
+
+/**
+ * @Author: Panson
+ */
+public class Youggc {
+
+    public static void main(String[] args) {
+        byte[] array1 = new byte[1024 * 1024];
+        array1 = new byte[1024 * 1024];
+        array1 = new byte[1024 * 1024];
+        array1 = null;
+
+        byte[] array2 = new byte[2 * 1024 * 1024];
+    }
+
+}
